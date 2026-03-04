4b0340208044f01a2ee26dc73d0749289bb12b3b
josemicode <jmiguelojedagutierrez@gmail.com>
2026-03-04 23:44:54 +0100

    Create test_invoice_line.py

---
+++ part_2/test_invoice_line.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class PartialBillingStub:
+    pass
+
+
+class CreditNoteBillItemStub:
+    pass
+
+
+class PriceAdjustmentBillItem:
+    pass
+
+
+class TestInvoiceLine(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_partial_billing(self) -> None:
+        pass
