8694a72df2bd366d5f9fbb1ee132782901ef450b
josemicode <jmiguelojedagutierrez@gmail.com>
2026-03-06 20:29:03 +0100

    Create test_provider.py

---
+++ part_2/test_provider.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class InvoiceStub:
+    def __init__(self) -> None:
+        pass
+
+
+class TestProvider(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
