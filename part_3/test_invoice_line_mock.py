568d4850f0dbb64f989729f22308aebd2d6b61fa
josemicode <jmiguelojedagutierrez@gmail.com>
2026-03-08 23:05:42 +0100

    setup left undone

--- part_3/test_invoice_line_mock.py
+++ part_3/test_invoice_line_mock.py
@@ -1,7 +1,8 @@
 import unittest
 from decimal import Decimal
+from unittest.mock import Mock
 
-from lab_2_part_3 import InvoiceLine
+from lab_2_part_3 import CreditNoteBillItem, InvoiceLine, PartialBilling
 
 
 class TestInvoiceLine(unittest.TestCase):
@@ -19,3 +20,113 @@ class TestInvoiceLine(unittest.TestCase):
             unitPriceEURPerKg=Decimal(7.5),
             qtyKg=Decimal(0),
         )
+
+        self.partial_not_billed = Mock(spec=PartialBilling)
+        self.partial_not_billed.billedKg = Decimal(0)
+
+        self.partial_20_billed = Mock(spec=PartialBilling)
+        self.partial_20_billed.billedKg = Decimal(20)
+
+        # self.credit_note_no_type = CreditNoteBillItemStub(self.invoice_line_zero_qty)
+        self.credit_note_no_type = Mock(spec=CreditNoteBillItem)
+        self.credit_note_no_type.target = self.invoice_line_zero_qty
+        self.credit_note_no_type.typeDeltaKg = Decimal(0)
+
+        # self.credit_note_10_type = CreditNoteBillItemStub(
+        #     self.invoice_line_zero_qty, Decimal(10)
+        # )
+        self.credit_note_10_type = Mock(spec=CreditNoteBillItem)
+        self.credit_note_10_type.target = self.invoice_line_zero_qty
+        self.credit_note_10_type.typeDeltaKg = Decimal(10)
+
+        # TODO: follow up!
+        self.price_adj_zero = PriceAdjustmentBillItemStub(self.invoice_line_zero_qty)
+        self.price_adj_1p5 = PriceAdjustmentBillItemStub(
+            self.invoice_line_zero_qty, Decimal(1.5)
+        )
+        self.price_adj_2 = PriceAdjustmentBillItemStub(
+            self.invoice_line_zero_qty, Decimal(2.0)
+        )
+
+    def test_add_partial_billing(self) -> None:
+        self.invoice_line_20_qty.add_partial_billing(self.partial_not_billed)
+        self.assertIn(self.partial_not_billed, self.invoice_line_20_qty.partials)
+
+    def test_add_another_partial_billing(self) -> None:
+        self.invoice_line_20_qty.add_partial_billing(self.partial_not_billed)
+        self.invoice_line_20_qty.add_partial_billing(self.partial_20_billed)
+
+        self.assertIn(self.partial_not_billed, self.invoice_line_20_qty.partials)
+        self.assertIn(self.partial_20_billed, self.invoice_line_20_qty.partials)
+
+    def test_add_credit_note_item(self) -> None:
+        self.invoice_line_20_qty.add_credit_note_item(self.credit_note_no_type)
+        self.assertIn(
+            self.credit_note_no_type, self.invoice_line_20_qty.credit_note_items
+        )
+        self.assertEqual(self.credit_note_no_type.target, self.invoice_line_20_qty)
+
+    def test_add_another_credit_note_item(self) -> None:
+        self.invoice_line_20_qty.add_credit_note_item(self.credit_note_no_type)
+        self.invoice_line_20_qty.add_credit_note_item(self.credit_note_10_type)
+
+        self.assertIn(
+            self.credit_note_no_type, self.invoice_line_20_qty.credit_note_items
+        )
+        self.assertIn(
+            self.credit_note_10_type, self.invoice_line_20_qty.credit_note_items
+        )
+
+    def test_add_price_adjustment_item(self) -> None:
+        self.invoice_line_20_qty.add_price_adjustment_item(self.price_adj_zero)
+        self.assertIn(
+            self.price_adj_zero, self.invoice_line_20_qty.price_adjustment_items
+        )
+        self.assertEqual(self.price_adj_zero.target, self.invoice_line_20_qty)
+
+    def test_add_another_price_adjustment_item(self) -> None:
+        self.invoice_line_20_qty.add_price_adjustment_item(self.price_adj_zero)
+        self.invoice_line_20_qty.add_price_adjustment_item(self.price_adj_2)
+
+        self.assertIn(
+            self.price_adj_zero, self.invoice_line_20_qty.price_adjustment_items
+        )
+        self.assertIn(self.price_adj_2, self.invoice_line_20_qty.price_adjustment_items)
+
+    def test_kilos_to_bill_no_remains(self) -> None:
+        self.invoice_line_20_qty.add_partial_billing(self.partial_20_billed)
+        self.invoice_line_20_qty.add_credit_note_item(self.credit_note_no_type)
+
+        result = self.invoice_line_20_qty.kilos_to_bill()
+        self.assertEqual(result, 0)
+
+    def test_kilos_to_bill_negative_remains(self) -> None:
+        self.invoice_line_zero_qty.add_partial_billing(self.partial_20_billed)
+        self.invoice_line_zero_qty.add_credit_note_item(self.credit_note_10_type)
+
+        result = self.invoice_line_zero_qty.kilos_to_bill()
+        self.assertEqual(result, 0)
+
+    def test_kilos_to_bill_remains(self) -> None:
+        self.invoice_line_20_qty.add_partial_billing(self.partial_20_billed)
+        self.invoice_line_20_qty.add_credit_note_item(self.credit_note_10_type)
+
+        result = self.invoice_line_20_qty.kilos_to_bill()
+        self.assertEqual(result, 10)
+
+    def test_unit_price_unchanged(self) -> None:
+        self.invoice_line_20_qty.add_price_adjustment_item(self.price_adj_zero)
+
+        unit_price = self.invoice_line_20_qty.unit_price()
+        self.assertEqual(unit_price, 2)
+
+    def test_unit_price_updates(self) -> None:
+        self.invoice_line_zero_qty.add_price_adjustment_item(self.price_adj_1p5)
+
+        unit_price = self.invoice_line_zero_qty.unit_price()
+        self.assertEqual(unit_price, 9)
+
+        self.invoice_line_zero_qty.add_price_adjustment_item(self.price_adj_2)
+
+        unit_price = self.invoice_line_zero_qty.unit_price()
+        self.assertEqual(unit_price, 11)
