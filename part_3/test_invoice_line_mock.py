25944178d80a275cd8ced01b3ac659663bfe75f4
josemicode <jmiguelojedagutierrez@gmail.com>
2026-03-08 19:43:38 +0100

    Update test_invoice_line_mock.py

--- part_3/test_invoice_line_mock.py
+++ part_3/test_invoice_line_mock.py
@@ -1,6 +1,21 @@
 import unittest
+from decimal import Decimal
+
+from lab_2_part_3 import InvoiceLine
 
 
 class TestInvoiceLine(unittest.TestCase):
     def setUp(self) -> None:
-        return super().setUp()
+        self.invoice_line_20_qty = InvoiceLine(
+            seq=1,
+            description="Line X",
+            unitPriceEURPerKg=Decimal(2.0),
+            qtyKg=Decimal(20),
+        )
+
+        self.invoice_line_zero_qty = InvoiceLine(
+            seq=2,
+            description="Line D",
+            unitPriceEURPerKg=Decimal(7.5),
+            qtyKg=Decimal(0),
+        )
