fb51e0674e24b64130d31d8b7eea8477f61ea9fd
PCRDO\walls <wallstreetdoggo@gmail.com>
2026-03-09 19:01:30 +0100

    Tested TestInvoice with mocks

--- part_3/test_invoice_mock.py
+++ part_3/test_invoice_mock.py
@@ -1,9 +1,32 @@
 import unittest
+from datetime import date
+from decimal import Decimal
+from unittest.mock import Mock
+
+from lab_2_part_3 import Invoice, InvoiceLine
 
 
 class TestInvoice(unittest.TestCase):
     def setUp(self) -> None:
-        return super().setUp()
+        self.invoice_eur = Invoice(number="INV-013", date=date.today(), currency="EUR")
+
+        self.inv_line_50_kg = Mock(spec=InvoiceLine)
+        self.inv_line_50_kg.seq = 1
+        self.inv_line_50_kg.description = "Line C"
+        self.inv_line_50_kg.unitPriceEURPerKg = Decimal(1.0)
+        self.inv_line_50_kg.qtyKg = Decimal(50.0)
+        self.inv_line_50_kg.lineAmount = Decimal(10.0)
+        self.inv_line_50_kg.kilos_to_bill.return_value = Decimal(50.0)
+        self.inv_line_50_kg.unit_price.return_value = Decimal(1.0)
+
+        self.inv_line_100_kg_price_x2 = Mock(spec=InvoiceLine)
+        self.inv_line_100_kg_price_x2.seq = 2
+        self.inv_line_100_kg_price_x2.description = "Line D"
+        self.inv_line_100_kg_price_x2.unitPriceEURPerKg = Decimal(2.0)
+        self.inv_line_100_kg_price_x2.qtyKg = Decimal(100.0)
+        self.inv_line_100_kg_price_x2.lineAmount = Decimal(10.0)
+        self.inv_line_100_kg_price_x2.kilos_to_bill.return_value = Decimal(100.0)
+        self.inv_line_100_kg_price_x2.unit_price.return_value = Decimal(2.0)
 
     def test_add_line(self) -> None:
         self.invoice_eur.add_line(self.inv_line_50_kg)
